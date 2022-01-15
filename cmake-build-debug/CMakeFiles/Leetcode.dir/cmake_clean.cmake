5b557246d1f2dbc90ad60c1838a5285779eeef2b
Yitong999 <nb.yitong@gmail.com>
2022-01-14 18:07:08 -0800

    upd

---
+++ cmake-build-debug/CMakeFiles/Leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Leetcode.dir/main.cpp.obj"
+  "CMakeFiles/Leetcode.dir/main.cpp.obj.d"
+  "Leetcode.exe"
+  "Leetcode.exe.manifest"
+  "Leetcode.pdb"
+  "libLeetcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
